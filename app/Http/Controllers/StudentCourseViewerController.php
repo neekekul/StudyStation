b8d728db98c5e7cadba03a0c5111d62a57ab6569
Austin <Stephen.sallee698@topper.wku.edu>
2018-04-13 12:54:07 -0500

    Student course viewer done. Ready to only display courses from followed instructors.

--- app/Http/Controllers/StudentCourseViewerController.php
+++ app/Http/Controllers/StudentCourseViewerController.php
@@ -12,7 +12,7 @@ class StudentCourseViewerController extends Controller
 
     }
   protected function getCourses(){
-    $courses = Course::select('name', 'instructor_id', 'id')->where('instructor_id','>','-1') -> get();
+    $courses = Course::select('name', 'instructor_id','username')-> join('users', 'courses.instructor_id', '=', 'users.id') ->where('instructor_id','>','-1') -> get();
     return view('studentCourseViewer',compact('courses'));
   }
 }
